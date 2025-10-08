e0f1b79153517084a987b51f789e256bc1f8287e
Jon Geiger Gajate <jongg97@gmail.com>
2025-10-08 21:20:23 +0200

    .

--- Neural Networks/CNN.py
+++ Neural Networks/CNN.py
@@ -1,61 +1,59 @@
 import numpy as np
+from multiprocessing import Manager, Process, freeze_support
 
 class CNN:
-    def __init__(self, noOfFilters, filterSize, stepSize, pFilterSize, pStepSize, learningRate, mode):
-        assert mode == "Classifier" or mode == "Regression", "unknown mode"
+    # config = filterSizes, stepSizes, pfilterSizes, pstepSizes (each as a list)
+    
+    def __init__(self, config, learningRate, mode, noOfFilterAndPoolingCombinations, filterLearningrate):
+        assert mode in ["Classifier", "Regression"], "unknown mode"
         self.mode = mode
-        self.noOfFilters = noOfFilters
-        self.filterSize = filterSize
-        self.stepSize = stepSize
+        self.noOfFilters, self.filterSizes, self.stepSizes, self.pfilterSizes, self.pstepSizes = config
         self.learningRate = learningRate
-        self.pFilterSize = pFilterSize
-        self.pStepSize = pStepSize
         self.weights = None
         self.bias = None
+        self.layers = []
+        for i in range(noOfFilterAndPoolingCombinations):
+            self.layers.append(FilterLayer(self.noOfFilters[i], self.filterSizes[i], self.stepSizes[i], filterLearningrate))
+            self.layers.append(PoolingLayer(self.pfilterSizes[i], self.pstepSizes[i]))
+        
 
     def fit(self, images, classes, epochs):
-        self.fl = FilterLayer(self.noOfFilters, self.filterSize, self.stepSize)
-        self.pl = PoolingLayer(self.pFilterSize, self.pStepSize)
-        
         self.uniqueClasses = len(np.unique(classes))
-
+        
         for epoch in range(epochs):
             for idx in range(len(images)):
-                pred, error, flattened, preFlattenedDimensions = self.__forward(images[idx], classes[idx], len(set(classes)))
-                self.__backward(pred, error, flattened, preFlattenedDimensions)
+                pred, error, flattened, preFlattenedDimensions = self.__forward(images[idx], classes[idx], self.uniqueClasses)
+                self.__backward(error, flattened, preFlattenedDimensions)
+                    
 
     def predict(self, image):
-        pred, _, _, _ = self.__forward(image, 1, self.uniqueClasses)
+        pred, _, _, _ = self.__forward(image, 0, self.uniqueClasses)
         return np.argmax(pred, axis=0)
 
     def __forward(self, image, actualClass, numClasses):
-        f = self.fl.forward(image)
-        f = self.pl.forward(f)
-
+        f = image
+        for l in self.layers:
+            f = l.forward(f)
         preFlattenDimensions = f.shape
         actualClassVec = np.eye(numClasses)[actualClass]
         flattened = f.flatten()
-
         if self.weights is None:
-            self.weights = np.random.rand(numClasses, len(flattened))
-            self.bias = np.random.rand(numClasses)
-
+            self.weights = np.random.rand(numClasses, len(flattened)) * 0.1
+            self.bias = np.random.rand(numClasses) * 0.1
         prediction = self.__softMax(flattened) if self.mode == "Classifier" else np.dot(self.weights, flattened) + self.bias
         error = prediction - actualClassVec
-
         return prediction, error, flattened, preFlattenDimensions
 
-    def __backward(self, pred, error, flattened, dimensionsPreFlattened):
+    def __backward(self, error, flattened, dimensionsPreFlattened):
         dW = np.outer(error, flattened)
-        dB = error
+        
         self.weights -= self.learningRate * dW
         self.bias -= self.learningRate * error
-
         deltaFlattened = np.dot(self.weights.T, error)
         deltaPooled = np.reshape(deltaFlattened, dimensionsPreFlattened)
-
-        deltaRelu = self.pl.backward(deltaPooled)
-        self.fl.backward(deltaRelu)
+        f = deltaPooled
+        for l in reversed(self.layers):
+            f = l.backward(f)
 
     def __softMax(self, x):
         totals = np.dot(self.weights, x) + self.bias
@@ -65,89 +63,94 @@ class CNN:
 
 
 class FilterLayer:
-    def __init__(self, noOfFilters, filterSize, stepSize):
-        self.filters = [np.random.rand(filterSize, filterSize)*0.1 for _ in range(noOfFilters)]
+    def __init__(self, noOfFilters, filterSize, stepSize, learningRate):
+        self.filters = None
+        self.noOfFilters = noOfFilters
         self.filterSize = filterSize
         self.stepSize = stepSize
+        self.learningRate = learningRate
 
     def forward(self, input):
+        if input.ndim == 2:
+            input = np.expand_dims(input, axis=0)
+        depth = input.shape[0]
+        if self.filters is None:
+            self.filters = [np.random.rand(depth, self.filterSize, self.filterSize)*0.1 for _ in range(self.noOfFilters)]
         self.prefiltered = input
-        self.activated = [self.__relu(self.__applyFilter(input, f)) for f in self.filters]
-        return np.array(self.activated)
+        size = (input.shape[1] - self.filterSize) // self.stepSize + 1
+        self.activated = np.array([self.__relu(self.__applyFilter(input, f, size))for f in self.filters])
+        return self.activated
 
-    def __applyFilter(self, input, filter):
-        size = (input.shape[0] - self.filterSize) // self.stepSize + 1
+    def backward(self, deltaRelu):
+        grad_input = np.zeros_like(self.prefiltered)
+        grad_filters = [np.zeros_like(f) for f in self.filters]
+        size = (self.prefiltered.shape[1] - self.filterSize) // self.stepSize + 1
+        
+        for idx, (filter, activation) in enumerate(zip(self.filters, self.activated)):
+            relu_mask = activation > 0
+            delta = deltaRelu[idx] * relu_mask
+            
+            for i in range(size):
+                for j in range(size):
+                    region = self.prefiltered[:, i*self.stepSize:i*self.stepSize+self.filterSize,
+                                              j*self.stepSize:j*self.stepSize+self.filterSize]
+                    grad_filters[idx] += delta[i, j] * region
+                    grad_input[:, i*self.stepSize:i*self.stepSize+self.filterSize,
+                                  j*self.stepSize:j*self.stepSize+self.filterSize] += delta[i, j] * filter
+            self.filters[idx] -= self.learningRate* grad_filters[idx]
+        return grad_input
+
+    def __relu(self, x):
+        return np.maximum(x, 0)
+
+    def __applyFilter(self, input, filter, size):
+        
         output = np.zeros((size, size))
-
         for i in range(size):
             for j in range(size):
-                region = input[i*self.stepSize:i*self.stepSize+self.filterSize,
-                               j*self.stepSize:j*self.stepSize+self.filterSize]
+                region = input[:, i*self.stepSize:i*self.stepSize+self.filterSize,
+                                  j*self.stepSize:j*self.stepSize+self.filterSize]
                 output[i, j] = np.sum(region * filter)
 
         return output
 
-    def __relu(self, input):
-        return np.maximum(input, 0)
-
-    def backward(self, deltaRelu):
-        for idx, (filter, activation) in enumerate(zip(self.filters, self.activated)):
-            reluMask = activation > 0
-            delta = deltaRelu[idx] * reluMask
-
-            size = (self.prefiltered.shape[0] - self.filterSize) // self.stepSize + 1
-            for i in range(size):
-                for j in range(size):
-                    region = self.prefiltered[i*self.stepSize:i*self.stepSize+self.filterSize,
-                                              j*self.stepSize:j*self.stepSize+self.filterSize]
-                    filter += delta[i, j] * region
-
 
 class PoolingLayer:
-    def __init__(self, filterSize, stepSize):
-        self.filterSize = filterSize
-        self.stepSize = stepSize
+    def __init__(self, filterSizes, stepSizes):
+        self.filterSizes = filterSizes
+        self.stepSizes = stepSizes
 
     def forward(self, input):
         self.memo = []
         self.reluInput = input
         pooled = []
-
+        
         for m in input:
             pooledMap, memoMap = self.__applyPooling(m)
             pooled.append(pooledMap)
             self.memo.append(memoMap)
-
         return np.array(pooled)
 
     def __applyPooling(self, input):
-        if input.shape[0] < self.filterSize or input.shape[1] < self.filterSize:
-            return input, np.zeros_like(input, dtype=object)
-
-        size = (input.shape[0] - self.filterSize) // self.stepSize + 1
+        size = (input.shape[0] - self.filterSizes) // self.stepSizes + 1
         output = np.zeros((size, size))
         memo = np.zeros((size, size), dtype=object)
-
         for i in range(size):
             for j in range(size):
-                region = input[i*self.stepSize:i*self.stepSize+self.filterSize,
-                               j*self.stepSize:j*self.stepSize+self.filterSize]
+                region = input[i*self.stepSizes:i*self.stepSizes+self.filterSizes,
+                               j*self.stepSizes:j*self.stepSizes+self.filterSizes]
                 maxIndex = np.argmax(region)
                 maxPos = np.unravel_index(maxIndex, region.shape)
-                memo[i, j] = (i*self.stepSize + maxPos[0], j*self.stepSize + maxPos[1])
+                memo[i, j] = (i*self.stepSizes + maxPos[0], j*self.stepSizes + maxPos[1])
                 output[i, j] = region[maxPos]
-
         return output, memo
 
     def backward(self, deltaPooled):
         deltaRelu = np.zeros_like(self.reluInput)
-
         for idx, memoMap in enumerate(self.memo):
             for i in range(memoMap.shape[0]):
                 for j in range(memoMap.shape[1]):
                     maxPos = memoMap[i, j]
                     deltaRelu[idx][maxPos] = deltaPooled[idx][i, j]
-
         return deltaRelu
-                
-          
+    
